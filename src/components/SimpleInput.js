b1548541b5cbb7438428baed81e7beee3c9be1b8
Hari krishna Ayyappa Narina <krishnanarina8@gmail.com>
2022-10-26 21:53:46 -0400

    addedemaiinput

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -5,17 +5,23 @@ const SimpleInput = (props) => {
   const [enteredValue, setValue] = useState("")
   const [touched, setTouched] = useState(false);
 
+  const [enteredemailValue, setemailValue] = useState("");
+  const [emailTouched, setemailTouched] = useState(false);
+
   const valid = enteredValue.trim()!=="";
   const nameinputisinvalid = !valid && touched;
+
+  const emailValid = enteredemailValue.trim()!=="" && enteredemailValue.includes("@");
+  const emailinputisinvalid = !emailValid && emailTouched;
 
   // If there are muntiple inputs we need to check the inputvalidity of all the inputs
   // Threfore we can use the below syntax
 
-  // let formisvalid = false;
+  let formisvalid = false;
 
-  // if (valid){
-  //   formisvalid = true;
-  // }
+  if (valid && emailValid){
+    formisvalid = true;
+  }
 
   const inputChangehandler = (event) => {
     const inputValue = event.target.value;
@@ -24,29 +30,42 @@ const SimpleInput = (props) => {
   
   const handleSubmit = (event) => {
     event.preventDefault();
-    setTouched(true);
-    if (!valid) {
-      return;
-    } 
     setValue("");
+    setemailValue("");
     setTouched(false);
+    setemailTouched(false);
   } 
 
   const inputfocushandler =  (event) => {
     setTouched(true);
   }
 
-  const formStyle = nameinputisinvalid ? 'form-control invalid' : 'form-control';
+  const inputemailchangeHandler = (event) => {
+    const input = event.target.value;
+    setemailValue(input);
+  }
+
+  const emailFocushandler = (event) => {
+    setemailTouched(true);
+  }
+
+  const formStylename = nameinputisinvalid ? 'form-control invalid' : 'form-control';
+  const formstyleemail = emailinputisinvalid ? 'form-control invalid': 'form-control';
   
   return (
     <form onSubmit={handleSubmit}>
-      <div className={formStyle}>
+      <div className={formStylename}>
         <label htmlFor='name'>Your Name</label>
         <input type='text' id='name' value = {enteredValue} onChange = {inputChangehandler} onBlur = {inputfocushandler}/>
         {nameinputisinvalid && <p className = "error-text">The field should not be emplty</p>}
       </div>
+      <div className={formstyleemail}>
+        <label htmlFor='email'>Email Address</label>
+        <input type='email' id='email' value = {enteredemailValue} onChange = {inputemailchangeHandler} onBlur = {emailFocushandler}/>
+        {emailinputisinvalid && <p className = "error-text">pleas eneter a valid email</p>}
+      </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled = {!formisvalid} >Submit</button>
       </div>
     </form>
   );
